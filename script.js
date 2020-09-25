419fd1c7c8375ad7e072ad6fc43a5d1bcebe3781
SamheinDM <SamheinDM@yandex.ru>
2020-09-25 14:57:33 +0300

    init: start Calculator task

--- script.js
+++ script.js
@@ -1,94 +1,92 @@
-const numbers = document.querySelectorAll('.number');
 const operations = document.querySelectorAll('.operator');
-const clearBtns = document.querySelectorAll('.clear-btn');
-const decimalBtn = document.getElementById('decimal');
-const result = document.getElementById('result');
+const numbers = document.querySelectorAll('.number');
+const clear = document.querySelectorAll('.clear-btn');
+const decimal = document.getElementById('decimal');
 const display = document.getElementById('display');
-let MemoryCurrentNumber = 0;
-let MemoryNewNumber = false;
-let MemoryPendingOperation = '';
 
-for (var i = 0; i < numbers.length; i++) {
-  var number = numbers[i];
-  number.addEventListener('click', function (e) {
-    numberPress(e.target.textContent);
-  });
+var CurrentNumberMemory = 0;
+var NewNumber = true;
+var CurrentOperation = '';
+
+for (let i = 0; i < operations.length; i++) {
+    var operation = operations[i];
+    operation.addEventListener('click', function (e) {
+        operationClick(e.target.textContent);
+    })
 }
 
-for (var i = 0; i < operations.length; i++) {
-  var operationBtn = operations[i];
-  operationBtn.addEventListener('click', function (e) {
-    operationPress(e.target.textContent);
-  });
+for (let i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    number.addEventListener('click', function (e) {
+        numberClick(e.target.textContent);
+    })
 }
 
-for (var i = 0; i < clearBtns.length; i++) {
-  var clearBtn = clearBtns[i];
-  clearBtn.addEventListener('click', function (e) {
-    clear(e.target.textContent);
-  });
+for (let i = 0; i < clear.length; i++) {
+    const cl = clear[i];
+    cl.addEventListener('click', function (e) {
+        clearClick(e.target.textContent);
+    })
 }
 
-decimalBtn.addEventListener('click', decimal);
+decimal.addEventListener('click', function (e) {
+    decimalClick(e.target.textContent);
+})
 
-function numberPress(number) {
-  if (MemoryNewNumber) {
-    display.value = number;
-    MemoryNewNumber = false;
-  } else {
-    if (display.value === '0') {
-      display.value = number;
+function operationClick(op) {
+    NewNumber = true;
+    if (CurrentOperation === '') {
+        CurrentOperation = op;
+    } else if (CurrentOperation === '+') {
+        CurrentNumberMemory += parseFloat(display.value);
+        display.value = CurrentNumberMemory;
+        CurrentOperation = '';
+    } else if (CurrentOperation === '-') {
+        CurrentNumberMemory -= display.value;
+        display.value = CurrentNumberMemory;
+        CurrentOperation = '';
+    } else if (CurrentOperation === '/') {
+        CurrentNumberMemory /= display.value;
+        display.value = CurrentNumberMemory;
+        CurrentOperation = '';
+    } else if (CurrentOperation === '*') {
+        CurrentNumberMemory *= display.value;
+        display.value = CurrentNumberMemory;
+        CurrentOperation = '';
     } else {
-      display.value += number;
+        CurrentOperation = '';
     }
-  }
+    console.log('Нажата операция ' + op);
 }
 
-function operationPress(op) {
-  let localOperationMemory = display.value;
-
-  if (MemoryNewNumber && MemoryPendingOperation !== '=') {
-    display.value = MemoryCurrentNumber;
-  } else {
-    MemoryNewNumber = true;
-    if (MemoryPendingOperation === '+') {
-      MemoryCurrentNumber += +localOperationMemory;
-    } else if (MemoryPendingOperation === '-') {
-      MemoryCurrentNumber -= +localOperationMemory;
-    } else if (MemoryPendingOperation === '*') {
-      MemoryCurrentNumber *= +localOperationMemory;
-    } else if (MemoryPendingOperation === '/') {
-      MemoryCurrentNumber /= +localOperationMemory;
+function numberClick(number) {
+    if (NewNumber === true) {
+        CurrentNumberMemory = parseFloat(display.value);
+        display.value = number;
     } else {
-      MemoryCurrentNumber = +localOperationMemory;
+        if (display.value == '0') {
+            display.value = number;
+        } else {
+            display.value += number;
+        }
     }
-    display.value = MemoryCurrentNumber;
-    MemoryPendingOperation = op;
-  }
+    NewNumber = false;
+    console.log('Нажат номер ' + number);
 }
 
-function decimal(argument) {
-  let localDecimalMemory = display.value;
-
-  if (MemoryNewNumber) {
-    localDecimalMemory = '0.';
-    MemoryNewNumber = false;
-  } else {
-    if (localDecimalMemory.indexOf('.') === -1) {
-      localDecimalMemory += '.';
+function clearClick(params) {
+    NewNumber = true;
+    display.value = 0;
+    if (params == 'c') {
+        CurrentOperation = '';
+        CurrentNumberMemory = 0;
     }
-  }
-  display.value = localDecimalMemory;
+    console.log('Нажат ' + params);
 }
 
-function clear(id) {
-  if (id === 'ce') {
-    display.value = '0';
-    MemoryNewNumber = true;
-  } else if (id === 'c') {
-    display.value = '0';
-    MemoryNewNumber = true;
-    MemoryCurrentNumber = 0;
-    MemoryPendingOperation = '';
-  }
+function decimalClick(dec) {
+    if (display.value.indexOf(dec) === -1) {
+        display.value += dec;
+    }
+    console.log('Нажата ' + dec); //ставить 0. если NewNumber = true и нажали сразу точку
 }
